c9f7a557ae216f7908401bd1d7c4d288499c9e01
WillyCillyLiao <xueyingliao2018@u.northwestern.edu>
2017-03-08 13:50:42 -0600

    Update table.h

--- table.h
+++ table.h
@@ -15,11 +15,31 @@ class Table {
 };
 #endif
 
+struct Cost{
+  unsigned preNode;
+  double cost;
+};
 
 #if defined(LINKSTATE)
 class Table {
   // Students should write this class
-
+  
+  // variables
+  unsigned src; // store the Start Node # (Each node has a Table object)
+  vector<unsigned> Nprime; // store the N'
+  map<unsigned, Cost> topo;  // unsigned: Destination Node #
+                             // type Cost: Predecessor Node #, Total Cost
+    
+  // Constructor
+  Table();
+  Table(const Table&);
+  
+  // Function
+  void updateTopo(unsigned path, map<unsigned, Cost> topoFromPath);
+  void updatCost(unsigned path, double cost);
+  void LinkStateAlg();
+  
+  
  public:
   ostream & Print(ostream &os) const;
 };
